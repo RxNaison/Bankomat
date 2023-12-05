70fca271399c8391633219956eecde23a45acf6e
RxNaison <huiANDpenis>
2023-12-05 21:58:12 +0300

    first commit

---
+++ ConsoleApp7/Banc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Bankomat
+{
+    internal class Banc
+    {
+        private Dictionary<string, Client> _accounts = new Dictionary<string, Client>();
+
+        public void AddAccount(Client client)
+        {
+            _accounts.Add(client.Login, client);
+        }
+        public bool FindLogin(string tryLogin, ref Client client)
+        {
+            return _accounts.TryGetValue(tryLogin, out client);
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Bankomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            bool runApp = true;
+            try
+            {
+                while (runApp)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Здаров");
+                    Console.WriteLine("Выберите, что хотите сделать:\n1 - создать аккаунт\n2 - войти\nq - выйти");
+                    char choose = Console.ReadKey(true).KeyChar;
+                    switch (choose)
+                    {
+                        case '1':
+                            menu.CreateAccountMenu();
+                            break;
+                        case '2':
+                            menu.EnterInAccountMenu();
+                            break;
+                        case 'q':
+                            runApp = false;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Введено неверное значение");
+                            Console.ReadKey();
+                            break;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
